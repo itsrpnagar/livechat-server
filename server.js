9c1d3dcf57dc644473bb5f1dbd796955f9564465
itsrpnagar <itsrpnagar@gmail.com>
2026-06-11 00:33:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -184,5 +184,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
